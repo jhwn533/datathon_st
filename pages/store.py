add0f4c06d855f25d49d054562bc4c7356e0f54f
min0908 <94737255+min0908@users.noreply.github.com>
2023-05-16 11:32:45 +0900

    .

--- pages/store.py
+++ pages/store.py
@@ -6,7 +6,7 @@ import seaborn as sns
 from matplotlib.animation import FuncAnimation
 import matplotlib.dates as mdates
 
-st.title('승민님도 store 분석이면 여기에 같이 붙일까요..?')
+st.title('Analyze by Store')
 train = pd.read_parquet('data/train.parquet', engine = 'pyarrow')
 
 store_nbr = st.selectbox("상점 번호를 선택하세요", options=sorted(train['store_nbr'].unique()))
