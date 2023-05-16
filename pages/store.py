3a23654504f96939b7954404b3925f6ca8d1da69
jhwn533 <123198546+jhwn533@users.noreply.github.com>
2023-05-16 11:38:28 +0900

    Update store.py

--- pages/store.py
+++ pages/store.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
 from matplotlib.animation import FuncAnimation
 import matplotlib.dates as mdates
